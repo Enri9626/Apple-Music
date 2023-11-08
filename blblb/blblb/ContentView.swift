2696397e7464135f20d478c55fbd4e3769ef14fa
Enri9626 <enrico.galati96@gmail.com>
2023-11-08 17:15:21 +0100

    First comment

---
+++ blblb/blblb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  blblb
+//
+//  Created by Enrico Galati on 08/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ blblb/blblb/blblbApp.swift
@@ -0,0 +1,17 @@
+//
+//  blblbApp.swift
+//  blblb
+//
+//  Created by Enrico Galati on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct blblbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
